29b4b52c216159b23daee78f774fcf12a200736f
taiki16 <pontaimax16@gmail.com>
2024-11-07 10:24:36 +0900

    プロジェクト作成

---
+++ prmn2023Fall/src/main/java/com/example/prmn2023fall/Prmn2023FallApplication.java
@@ -0,0 +1,13 @@
+package com.example.prmn2023fall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prmn2023FallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2023FallApplication.class, args);
+    }
+
+}
---
+++ prmn2023Fall/src/test/java/com/example/prmn2023fall/Prmn2023FallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prmn2023fall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2023FallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
